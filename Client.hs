40e06e5f0d793e7fe5a7c0457651655c3f5346cd
Wei Qi <qiwei.uk@gmail.com>
2014-11-02 22:53:00 +0800

    wip

--- Client.hs
+++ Client.hs
@@ -1,5 +1,6 @@
 -- Client.hs
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
 
 import qualified Data.Map as M
 import qualified Data.Set as S
@@ -13,7 +14,8 @@ data Client i = GovOrg  { clientId :: i, clientName :: String }
 data Person = Person { firstName :: String, lastName :: String }
               deriving (Show, Ord, Eq)
 
-data ClientKind = GovOrgKind | CompanyKind | IndividualKind deriving (Show, Eq, Ord)
+data ClientKind = GovOrgKind | CompanyKind | IndividualKind
+                  deriving (Show, Eq, Ord)
 
 kind' :: Client Integer -> ClientKind
 kind' GovOrg{..}     = GovOrgKind
@@ -26,4 +28,20 @@ classifyClients  = foldr f M.empty
                   case M.lookup k m of
                     Just s -> M.insert k (S.insert c s) m
                     Nothing -> M.insert k (S.insert c S.empty) m
+
+
+classifyClients' :: [Client Integer] -> M.Map ClientKind (S.Set (Client Integer))
+classifyClients' cs =
+  let s1 = S.fromList $ filter (\case GovOrg{..} -> True
+                                      _          -> False) cs in
+  let s2 = S.fromList $ filter (\case Company{..} -> True
+                                      _           -> False) cs in
+  let s3 = S.fromList $ filter (\case Individual{..} -> True
+                                      _              -> False) cs in
+  let m1 = M.insert GovOrgKind s1 M.empty in
+  let m2 = M.insert CompanyKind s2 m1 in
+  let m3 = M.insert IndividualKind s3 m2 in
+  m3
+
+  
 
