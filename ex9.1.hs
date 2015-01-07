40861419cee8f42e97993777759120b1b77a5f85
Wei Qi <qiwei.uk@gmail.com>
2015-01-08 00:21:14 +0800

    improve ex9.1

--- ex9.1.hs
+++ ex9.1.hs
@@ -1,13 +1,27 @@
--- ex9.1.hs
+{-# LANGUAGE ScopedTypeVariables #-}
 
---guessOk :: (Int, Int) -> Int -> Bool
 import Control.Monad
+import System.Random
 
-guess :: Int -> IO Bool
+promptMessage :: Int -> IO ()
+promptMessage tries =
+  if tries < 5
+    then putStrLn "Not correct, try again!"
+    else putStrLn "You have used up opportunities, quitting..."
+
+guess :: Int -> Int -> IO Bool
 guess tries actual =
-  do
-    when (tries <= 5) $ do (g :: Int) <- fmap read getLine
-                           if g == actual
-                             then return True
-                             else guess (tries + 1) actual
+  if tries <= 5
+    then do (g :: Int) <- fmap read getLine
+            if g == actual
+              then return True
+              else promptMessage tries >> guess (tries + 1) actual
+    else return False
 
+main :: IO ()
+main =
+  do
+    putStrLn "Please guess a number between 1 and 10:"
+    rn <- randomRIO (1, 10)
+    b <- guess 1 rn
+    when b $ putStrLn $ "You are correct! The number is: " ++ show rn
